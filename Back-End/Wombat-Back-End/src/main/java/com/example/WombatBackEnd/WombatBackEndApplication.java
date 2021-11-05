f9f7cb518262f3aea8a66e695457ad56424b868e
omatography <omkarslam420@gmail.com>
2021-11-05 13:08:18 +0530

    initial commit with all things setup for development
    Initialized Frontend Backend and database.

---
+++ Back-End/Wombat-Back-End/src/main/java/com/example/WombatBackEnd/WombatBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.WombatBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WombatBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WombatBackEndApplication.class, args);
+	}
+
+}
