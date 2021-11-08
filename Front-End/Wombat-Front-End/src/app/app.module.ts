c7f2fd4d904b0b7c164f40365b52e76b3c949d6f
omatography <omkarslam420@gmail.com>
2021-11-08 18:09:13 +0530

    Home Component

--- Front-End/Wombat-Front-End/src/app/app.module.ts
+++ Front-End/Wombat-Front-End/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes:Routes = [
+  { path: '', pathMatch: 'full', component:LoginComponent},
+  {path:'home', component:HomeComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LoginComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
