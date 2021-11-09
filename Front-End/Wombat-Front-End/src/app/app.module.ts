5fef331aeaf8a7694db65ddeb711b29eded4c6cf
omatography <omkarslam420@gmail.com>
2021-11-09 16:59:29 +0530

    Question component

--- Front-End/Wombat-Front-End/src/app/app.module.ts
+++ Front-End/Wombat-Front-End/src/app/app.module.ts
@@ -6,10 +6,14 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { QuestionComponent } from './components/question/question.component';
+import { ResultComponent } from './components/result/result.component';
 
 const routes:Routes = [
   { path: '', pathMatch: 'full', component:LoginComponent},
-  {path:'home', component:HomeComponent }
+  {path:'home', component:HomeComponent },
+  {path:'question', component:QuestionComponent},
+  {path:'result', component:ResultComponent}
 ]
 
 @NgModule({
@@ -18,6 +22,8 @@ const routes:Routes = [
     NavbarComponent,
     LoginComponent,
     HomeComponent,
+    QuestionComponent,
+    ResultComponent,
   ],
   imports: [
     BrowserModule,RouterModule.forRoot(routes)
