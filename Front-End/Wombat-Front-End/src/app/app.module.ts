4f9a4addd644a01f0462b52c4f00e297690c0cf7
omatography <omkarslam420@gmail.com>
2021-11-10 15:29:52 +0530

    result section and about section

--- Front-End/Wombat-Front-End/src/app/app.module.ts
+++ Front-End/Wombat-Front-End/src/app/app.module.ts
@@ -8,12 +8,20 @@ import { HomeComponent } from './components/home/home.component';
 import { RouterModule, Routes } from '@angular/router';
 import { QuestionComponent } from './components/question/question.component';
 import { ResultComponent } from './components/result/result.component';
-
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { DetailedResultComponent } from './components/detailed-result/detailed-result.component';
+import { EditComponent } from './components/edit/edit.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 const routes:Routes = [
   { path: '', pathMatch: 'full', component:LoginComponent},
   {path:'home', component:HomeComponent },
   {path:'question', component:QuestionComponent},
-  {path:'result', component:ResultComponent}
+  {path:'result', component:ResultComponent},
+  {path:'detailed', component:DetailedResultComponent},
+  {path:'edit',component:EditComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent}
 ]
 
 @NgModule({
@@ -24,9 +32,33 @@ const routes:Routes = [
     HomeComponent,
     QuestionComponent,
     ResultComponent,
+    DetailedResultComponent,
+    EditComponent,
+    AboutComponent,
+    ContactComponent,
   ],
   imports: [
-    BrowserModule,RouterModule.forRoot(routes)
+    BrowserModule,RouterModule.forRoot(routes),
+    NgCircleProgressModule.forRoot({
+      // set defaults here
+      "backgroundColor": "#101010",
+      "radius": 60,
+      "maxPercent": 200,
+      "units": " Point",
+      "unitsColor": "#483500",
+      "outerStrokeWidth": 5,
+      "outerStrokeColor": "#FFFFFF",
+      "innerStrokeColor": "#FFFFFF",
+      "titleColor": "#ffffff",
+      "subtitleColor": "#ffffff",
+      "titleFontSize": "50",
+      "titleFontWeight": "500",
+      "showUnits": false,
+      "showSubtitle": false,
+      "showInnerStroke": false,
+      "startFromZero": false
+      
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Front-End/Wombat-Front-End/src/app/components/detailed-result/detailed-result.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DetailedResultComponent } from './detailed-result.component';
+
+describe('DetailedResultComponent', () => {
+  let component: DetailedResultComponent;
+  let fixture: ComponentFixture<DetailedResultComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DetailedResultComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DetailedResultComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
